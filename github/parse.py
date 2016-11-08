b301a541c88dcc55c2965fa79aa2d4cb54f7d083
Hebi Li <lihebi.com@gmail.com>
2016-11-08 17:24:49 -0600

    divide based on size

--- github/parse.py
+++ github/parse.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+"""
+Just run the script.
+It will parse the file c-[1-10].json.
+The output will be printed to the standard output.
+Write it to a file.
+"""
 import json
-
-
 
 if __name__ == '__main__':
     # the 0 is the same as 1
     # the valid data is from 1 to 10
     for i in range(1,11):
-        filename='c-' + str(i) + '.json'
+        filename='json/c-' + str(i) + '.json'
         j=json.load(open(filename))
         # print(len(j['items']))
         # https://github.com/lihebi/cs572-project/archive/master.zip
@@ -18,6 +22,15 @@ if __name__ == '__main__':
             # print(it['full_name'])
             fullname=it['full_name']
             star=it['stargazers_count']
+            size=it['size']
             outputname=fullname.replace('/','--')
             # print(fullname + ' ' + str(star))
-            print(prefix + fullname + suffix + ' -O ' + outputname + '.zip')
+            if size < 9999:
+                print(prefix + fullname + suffix + ' -O ' + outputname + '.zip')
+                pass
+            elif size < 99999:
+                # print(prefix + fullname + suffix + ' -O ' + outputname + '.zip')
+                pass
+            else:
+                # print(prefix + fullname + suffix + ' -O ' + outputname + '.zip')
+                pass

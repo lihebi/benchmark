5e252779e939c428c0312b28a93a45d8c3a158de
Hebi Li <lihebi.com@gmail.com>
2017-03-29 03:09:26 -0500

    add craft

---
+++ craft/grammar/a.c
@@ -0,0 +1,19 @@
+struct A {
+  int a;
+  int b;
+};
+
+int foo() {
+  int a,b,c,d,e;
+  A aa;
+  while (a<c) {
+    a=b;
+    if (b>0) {
+      d=c+e;
+    } else if (b < 10) {
+      e+=c+b;
+      a=8;
+    }
+    a++;
+  }
+}
